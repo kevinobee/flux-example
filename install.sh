48655ea2944dbf757713b6c4f386466b2641668d
Kevin Obee <kevin.obee@gmail.com>
2022-05-14 12:12:56 +0100

    setup port-forwarding to infrastructure dashboards

--- install.sh
+++ install.sh
@@ -76,6 +76,26 @@ flux bootstrap github \
   --personal
 
 echo
+echo "Wait for infrastructure to be ready ..."
+kubectl -n flux-system wait kustomization/infrastructure --for=condition=ready --timeout=5m
+kubectl -n flux-system wait kustomization/finalizers --for=condition=ready --timeout=10m
+
+# Expose Kynervo Policy Reporter
+if [[ ! $(netstat -tlp | grep kubectl | grep "localhost:8082") ]]; then
+  kubectl -n policy-reporter port-forward service/policy-reporter-policy-reporter-ui 8082:8080 > /dev/null 2>&1 &
+fi
+
+# Expose Linkerd-Viz dashboards
+if [[ ! $(netstat -tlp | grep kubectl | grep "localhost:8084") ]]; then
+  kubectl -n linkerd-viz port-forward svc/web 8084:8084 > /dev/null 2>&1 &
+fi
+
+# Expose Grafana dashboards
+if [[ ! $(netstat -tlp | grep kubectl | grep "localhost:3000") ]]; then
+  kubectl -n monitoring port-forward svc/kube-prometheus-stack-grafana 3000:80 > /dev/null 2>&1 &
+fi
+
+echo
 flux tree kustomization flux-system --compact
 echo
 flux get all
@@ -87,12 +107,6 @@ echo
 echo "Watch status of all Flux resources:"
 echo
 echo "watch flux get all"
-echo
-echo
-echo "Wait for all cluster infrastructure to be available:"
-echo
-echo "kubectl -n flux-system wait kustomization/infrastructure --for=condition=ready"
-echo "kubectl -n flux-system wait kustomization/finalizers --for=condition=ready"
 echo
 echo
 echo "Force Flux reconciliation:"
@@ -107,8 +121,6 @@ echo
 echo
 echo "View Flux dashboard in Grafana:"
 echo
-echo "kubectl -n monitoring port-forward svc/kube-prometheus-stack-grafana 3000:80 > /dev/null 2>&1 &"
-echo
 echo "Flux Cluster Stats: http://localhost:3000/d/flux-cluster/flux-cluster-stats"
 echo "Flux Control Plane: http://localhost:3000/d/flux-control-plane"
 echo
@@ -118,12 +130,22 @@ echo
 echo "octant"
 echo
 echo
-echo "Run Linkerd UI:"
+echo "View Linkerd Service Mesh status:"
 echo
-echo "linkerd viz dashboard &"
+echo "Linkerd dashboard:  http://localhost:8084"
+echo "Grafana dashboard:  http://localhost:8084/grafana"
 echo
-echo "Linkerd dashboard: http://localhost:50750"
-echo "Grafana dashboard: http://localhost:50750/grafana"
+echo
+echo "View Kynervo Policy Reporter:"
+echo
+echo "Policy Reporter:    http://localhost:8082"
+echo
+echo
+echo "View Litmus ChaosCenter:"
+echo
+echo "kubectl -n litmus port-forward svc/chaos-litmus-frontend-service 9091:9091 > /dev/null 2>&1 &"
+echo
+echo "Litmus:             http://localhost:9091/"
 echo
 echo
 echo "Run Kubescape scan:"
