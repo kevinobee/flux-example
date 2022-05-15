6c504fe31f1d24ba99cd67260cff9d35d29e4e3b
Kevin Obee <kevin.obee@gmail.com>
2022-05-15 09:01:16 +0100

    add script to expose cluster dashboards

--- install.sh
+++ install.sh
@@ -76,26 +76,6 @@ flux bootstrap github \
   --personal
 
 echo
-echo "Wait for infrastructure to be ready ..."
-kubectl -n flux-system wait kustomization/infrastructure --for=condition=ready --timeout=5m
-kubectl -n flux-system wait kustomization/finalizers --for=condition=ready --timeout=10m
-
-# Expose Kynervo Policy Reporter
-if [[ ! $(netstat -tlp | grep kubectl | grep "localhost:8082") ]]; then
-  kubectl -n policy-reporter port-forward service/policy-reporter-policy-reporter-ui 8082:8080 > /dev/null 2>&1 &
-fi
-
-# Expose Linkerd-Viz dashboards
-if [[ ! $(netstat -tlp | grep kubectl | grep "localhost:8084") ]]; then
-  kubectl -n linkerd-viz port-forward svc/web 8084:8084 > /dev/null 2>&1 &
-fi
-
-# Expose Grafana dashboards
-if [[ ! $(netstat -tlp | grep kubectl | grep "localhost:3000") ]]; then
-  kubectl -n monitoring port-forward svc/kube-prometheus-stack-grafana 3000:80 > /dev/null 2>&1 &
-fi
-
-echo
 flux tree kustomization flux-system --compact
 echo
 flux get all
@@ -119,33 +99,14 @@ echo
 echo "flux tree kustomization flux-system --compact"
 echo
 echo
-echo "View Flux dashboard in Grafana:"
+echo "Setup port forwards to expose Cluster dashboards:"
 echo
-echo "Flux Cluster Stats: http://localhost:3000/d/flux-cluster/flux-cluster-stats"
-echo "Flux Control Plane: http://localhost:3000/d/flux-control-plane"
+echo "./port-forward.sh"
 echo
 echo
 echo "View the cluster configuration dashboard:"
 echo
 echo "octant"
-echo
-echo
-echo "View Linkerd Service Mesh status:"
-echo
-echo "Linkerd dashboard:  http://localhost:8084"
-echo "Grafana dashboard:  http://localhost:8084/grafana"
-echo
-echo
-echo "View Kynervo Policy Reporter:"
-echo
-echo "Policy Reporter:    http://localhost:8082"
-echo
-echo
-echo "View Litmus ChaosCenter:"
-echo
-echo "kubectl -n litmus port-forward svc/chaos-litmus-frontend-service 9091:9091 > /dev/null 2>&1 &"
-echo
-echo "Litmus:             http://localhost:9091/"
 echo
 echo
 echo "Run Kubescape scan:"
