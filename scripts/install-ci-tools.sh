7788037c6ac90b5646a110ad3439dc5cc3c3b6bc
Kevin Obee <kevin.obee@gmail.com>
2022-05-23 13:14:38 +0100

    add install ci tools script

---
+++ scripts/install-ci-tools.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# install-ci-tools.sh - installs CLI tools using HomeBrew for CI runners
+
+# standard bash error handling
+set -o errexit;
+set -o pipefail;
+set -o nounset;
+# debug commands
+# set -x;
+
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+
+brewTools=( \
+  "kind"
+)
+
+if [ ! $(which brew) ]; then
+  (
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  )
+fi
+
+for i in "${brewTools[@]}"
+do
+  if [[ ! $(which "${i}") ]]; then
+    brew install "${i}"
+  fi
+done
+
+if [[ ! $(which "flux") ]]; then
+  brew install "fluxcd/tap/flux"
+fi
