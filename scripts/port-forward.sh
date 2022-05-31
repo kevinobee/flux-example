6d6be61a9c74d207548fddd7cb4f2db0f538d27f
Kevin Obee <kevin.obee@gmail.com>
2022-05-31 08:26:34 +0100

    add comments

--- scripts/port-forward.sh
+++ scripts/port-forward.sh
@@ -14,26 +14,29 @@ echo
 echo "Wait for infrastructure to be ready ..."
 kubectl -n flux-system wait kustomization/infrastructure --for=condition=ready --timeout=5m
 kubectl -n flux-system wait kustomization/finalizers --for=condition=ready --timeout=10m
+# kubectl -n flux-system wait helmrelease/policy-reporter --for=condition=ready
+
 echo
 echo "Wait for tools to be ready ..."
 kubectl -n flux-system wait kustomization/tools --for=condition=ready --timeout=10m
+kubectl -n flux-system wait helmrelease/litmuschaos --for=condition=ready --timeout=10m
+
+# Expose monitoring Grafana on port 3000
+if [[ ! $(netstat -tlp | grep kubectl | grep "localhost:3000") ]]; then
+  kubectl -n monitoring port-forward svc/kube-prometheus-stack-grafana 3000:80 > /dev/null 2>&1 &
+fi
 
 # # Expose Kynervo Policy Reporter
 # if [[ ! $(netstat -tlp | grep kubectl | grep "localhost:8082") ]]; then
 #   kubectl -n policy-reporter port-forward service/policy-reporter-policy-reporter-ui 8082:8080 > /dev/null 2>&1 &
 # fi
 
-# Expose Linkerd-Viz dashboards
+# Expose Linkerd-Viz dashboards on port 8084
 if [[ ! $(netstat -tlp | grep kubectl | grep "localhost:8084") ]]; then
   kubectl -n linkerd-viz port-forward svc/web 8084:8084 > /dev/null 2>&1 &
 fi
 
-# Expose Grafana dashboards
-if [[ ! $(netstat -tlp | grep kubectl | grep "localhost:3000") ]]; then
-  kubectl -n monitoring port-forward svc/kube-prometheus-stack-grafana 3000:80 > /dev/null 2>&1 &
-fi
-
-# Expose Litmus ChaosCenter
+# Expose Litmus ChaosCenter on port 9091
 if [[ ! $(netstat -tlp | grep kubectl | grep "localhost:9091") ]]; then
   kubectl -n litmus port-forward svc/chaos-litmus-frontend-service 9091:9091 > /dev/null 2>&1 &
 fi
