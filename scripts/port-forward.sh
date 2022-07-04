2a5403ecd2c387869bd22a88cbcc0c520f4e3bef
Kevin Obee <kevin.obee@gmail.com>
2022-07-04 11:51:39 +0100

    expose prometheus

--- scripts/port-forward.sh
+++ scripts/port-forward.sh
@@ -8,22 +8,26 @@ set -o nounset;
 # debug commands
 # set -x;
 
-# Expose monitoring Grafana on port 3000
-if [[ ! $(netstat -tlp | grep kubectl | grep "localhost:3000") ]]; then
-  kubectl -n monitoring port-forward svc/kube-prometheus-stack-grafana 3000:80 > /dev/null 2>&1 &
+echo 'Expose Grafana on port 3000'
+if [[ ! $(netstat -tlp 2> /dev/null | grep kubectl | grep "localhost:3000") ]]; then
+  kubectl -n monitoring port-forward svc/kube-prometheus-stack-grafana 3000:80 > /dev/null &
 fi
 
-# # Expose Kynervo Policy Reporter
-# if [[ ! $(netstat -tlp | grep kubectl | grep "localhost:8082") ]]; then
-#   kubectl -n policy-reporter port-forward service/policy-reporter-policy-reporter-ui 8082:8080 > /dev/null 2>&1 &
-# fi
+# echo 'Expose Kynervo Policy Reporter on port 8082'
+# if [[ ! $(netstat -tlp 2> /dev/null | grep kubectl | grep "localhost:8082") ]]; then
+#   kubectl -n policy-reporter port-forward service/policy-reporter-policy-reporter-ui 8082:8080 > /dev/null &
 
-# Expose Emojivoto on port 8080
-if [[ ! $(netstat -tlp | grep kubectl | grep "localhost:8080") ]]; then
-  kubectl -n emojivoto port-forward svc/web-svc 8080:80 > /dev/null 2>&1 &
+echo 'Expose Emojivoto on port 8080'
+if [[ ! $(netstat -tlp 2> /dev/null | grep kubectl | grep "localhost:8080") ]]; then
+  kubectl -n emojivoto port-forward svc/web-svc 8080:80 > /dev/null > /dev/null &
 fi
 
-# Expose Litmus ChaosCenter on port 9091
-if [[ ! $(netstat -tlp | grep kubectl | grep "localhost:9091") ]]; then
-  kubectl -n litmus port-forward svc/litmus-litmuschaos-frontend-service 9091:9091 > /dev/null 2>&1 &
+echo 'Expose Litmus Chaos on port 9091'
+if [[ ! $(netstat -tlp 2> /dev/null | grep kubectl | grep "localhost:9091") ]]; then
+  kubectl -n litmus port-forward svc/litmus-litmuschaos-frontend-service 9091:9091 > /dev/null &
+fi
+
+echo 'Expose Prometheus on port 9090'
+if [[ ! $(netstat -tlp 2> /dev/null | grep kubectl | grep "localhost:9090") ]]; then
+  kubectl -n monitoring port-forward svc/kube-prometheus-stack-prometheus 9090:80 > /dev/null &
 fi
