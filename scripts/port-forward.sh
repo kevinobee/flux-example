7977e3aa92b424c857cb261fe6e34e819bfe9969
Kevin Obee <kevin.obee@gmail.com>
2022-07-06 09:13:39 +0100

    echo exposed service urls

--- scripts/port-forward.sh
+++ scripts/port-forward.sh
@@ -8,27 +8,27 @@ set -o nounset;
 # debug commands
 # set -x;
 
-echo 'Expose Grafana on port 3000'
+echo 'Expose Grafana on http://localhost:3000'
 if [[ ! $(netstat -tlp 2> /dev/null | grep kubectl | grep "localhost:3000") ]]; then
   kubectl -n monitoring port-forward svc/kube-prometheus-stack-grafana 3000:80 > /dev/null &
 fi
 
-echo 'Expose Emojivoto on port 8080'
+echo 'Expose Emojivoto on http://localhost:8080'
 if [[ ! $(netstat -tlp 2> /dev/null | grep kubectl | grep "localhost:8080") ]]; then
   kubectl -n emojivoto port-forward svc/web-svc 8080:80 > /dev/null > /dev/null &
 fi
 
-echo 'Expose Policy Reporter on port 8082'
+echo 'Expose Policy Reporter on http://localhost:8082'
 if [[ ! $(netstat -tlp 2> /dev/null | grep kubectl | grep "localhost:8082") ]]; then
   kubectl -n policy-reporter port-forward service/policy-reporter-policy-reporter-ui 8082:8080 > /dev/null &
 fi
 
-echo 'Expose Litmus Chaos on port 9091'
+echo 'Expose Litmus Chaos on http://localhost:9091'
 if [[ ! $(netstat -tlp 2> /dev/null | grep kubectl | grep "localhost:9091") ]]; then
   kubectl -n litmus port-forward svc/litmus-litmuschaos-frontend-service 9091:9091 > /dev/null &
 fi
 
-echo 'Expose Prometheus on port 9090'
+echo 'Expose Prometheus on http://localhost:9090'
 if [[ ! $(netstat -tlp 2> /dev/null | grep kubectl | grep "localhost:9090") ]]; then
   kubectl -n monitoring port-forward svc/kube-prometheus-stack-prometheus 9090:9090 > /dev/null &
 fi
