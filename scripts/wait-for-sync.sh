44e0d2285139dd805a1edbd66b6ae3c9deb2aa67
Kevin Obee <kevin.obee@gmail.com>
2022-07-26 09:24:10 +0100

    re-order waits

--- scripts/wait-for-sync.sh
+++ scripts/wait-for-sync.sh
@@ -17,10 +17,10 @@ kubectl -n flux-system	wait kustomization/linkerd --for=condition=ready --timeou
 kubectl -n flux-system	wait kustomization/policy --for=condition=ready --timeout=5m
 kubectl -n flux-system	wait kustomization/cert-manager --for=condition=ready --timeout=5m
 kubectl -n flux-system	wait kustomization/metallb --for=condition=ready --timeout=5m
-kubectl -n flux-system	wait kustomization/ingress-nginx --for=condition=ready --timeout=5m
+kubectl -n flux-system	wait kustomization/sealed-secrets --for=condition=ready --timeout=5m
 kubectl -n flux-system	wait kustomization/metrics-server --for=condition=ready --timeout=5m
 kubectl -n flux-system	wait kustomization/monitoring --for=condition=ready --timeout=5m
-kubectl -n flux-system	wait kustomization/sealed-secrets --for=condition=ready --timeout=5m
+kubectl -n flux-system	wait kustomization/ingress-nginx --for=condition=ready --timeout=5m
 kubectl -n flux-system	wait kustomization/finalizers --for=condition=ready --timeout=5m
 kubectl -n flux-system	wait kustomization/infrastructure --for=condition=ready --timeout=5m
 
